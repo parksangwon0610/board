4beec679e87a66cda54a59c31efcfb96567971d2
박상원 <sangwon@sungwon-iMac.local>
2020-10-29 23:19:18 +0900

    수정 / eslint 초기 적용

--- server.js
+++ server.js
@@ -1,32 +1,28 @@
 const express = require('express');
+
 const app = express();
 const port = 3000;
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const swaggerDocs = require('./lib/config/swagger');
 const sign = require('./lib/utils/signUtils');
+
 app.use(bodyParser.json());
 app.use(swaggerDocs);
 
 app.use(sign.checkApiKey);
 app.use('/users', require('./lib/routers/userRouter'));
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
-
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
 
 // database connection
-mongoose.connect('mongodb://localhost:27017/board',{
+mongoose.connect('mongodb://localhost:27017/board', {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
 });
 
 const db = mongoose.connection;
 db.on('error', console.error);
-db.once('open', function(){
-  console.log("Connection to mongoDB");
+db.once('open', () => {
+  console.log('Connection to mongoDB');
 });
-
-
-
-
-
